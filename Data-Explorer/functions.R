44eb405d9f324135b15c73d6ffb5826aa7f10e20
Ruth Gordon <ruthgo01@nssrstudio07.csa.scot.nhs.uk>
2022-02-02 09:00:06 +0000

    Updated scripts for the data explorer

--- Data-Explorer/functions.R
+++ Data-Explorer/functions.R
@@ -1,46 +1,32 @@
-################################################################
-### Functions
-### Data Explorer script 1 of 5
-###
-### Original Author: Jack Hannah
-### Original Date: 01 June 2018
-### Last edited by: Jack Hannah
-### Last edited on: 26 November 2018
-###
-### Written to be run on RStudio Desktop
-###
-### Packages required:
-### dplyr and tidyr (for data manipulation);
-### stringi (for string manipulation)
-###
-### This script defines several functions to be called
-### in the next script (data_preparation.R) which will
-### be used to manipulate and combine a series of base
-### files
+############################################################
+## Code name - functions.R
+## Data Release - Quarterly Data Explorer
+## Latest Update: Ruth Gordon, February 2022
+##
+## Written/run on - R Studio SERVER
+## R version - 3.6.1
+##
+## This script defines several functions to be called in the 
+## script 'data_preparation.R', which will be used to 
+## manipulate and combine a series of base files
 
-
-
+############################################################
 ### Section 1: Housekeeping ----
-
 
 # 1.1 - Load libraries
 library(dplyr)
 library(tidyr)
 library(stringi)
 
-
-
+############################################################
 ### Section 2: Functions ----
-
 
 # Section 2.1: Residence files ----
 # This function contains the data manipulation steps
 # to be applied to all residence (res) files
 res <- function(df) {
   df %>%
-    
-  # Exclude Scotland and Golden Jubilee
-  # to avoid duplicates when merging
+# Exclude Scotland and Golden Jubilee to avoid duplicates when merging
   filter(!(loc_name %in% c("Scotland",
                            "Golden Jubilee National Hospital"))) %>%
     mutate(geo_type = case_when(
@@ -52,8 +38,6 @@ res <- function(df) {
       ~ "NHS Board of residence"
     ))
 }
-
-
 
 # Section 2.2: Treatment files ----
 # This function contains the data manipulation steps
@@ -73,8 +57,6 @@ treat <- function(df) {
     ))
 }
 
-
-
 # Section 2.3: Combining inpatient files ----
 # This function contains the steps for combining
 # and manipulating inpatient files
@@ -82,12 +64,9 @@ comb_inp <- function(df_1, df_2) {
   bind_rows(df_1, df_2) %>%
     mutate(avlos = round(avlos, 1),
            file = "Inpatients/Day Cases") %>%
-    
-    # Remove a few random null measure cases
+# Remove a few random null measure cases
     drop_na(measure)
 }
-
-
 
 # Section 2.4: Combining outpatient files ----
 # This function contains the steps for combining
@@ -99,18 +78,14 @@ comb_outp <- function(df_1, df_2) {
            rate = round(rate, 1))
 }
 
-
-
 # Section 2.5: Combining inpatient and outpatient files ----
 # This function contains the steps for combining
 # the inpatient and outpatient files created using
 # the previous two functions
 comb_all <- function(df_1, df_2) {
   bind_rows(df_1, df_2) %>%
-    
-    # Recode measure into a more informative descrition
-    # and/or to remove capital letters from mid-sentence
-    # words
+# Recode measure into a more informative descrition and/or to 
+# remove capital letters from mid-sentence words
     mutate(measure = recode(
       measure,
       "DNA" = "Did not attend outpatient appointments",
@@ -126,10 +101,8 @@ comb_all <- function(df_1, df_2) {
     ))
 }
 
-
 # Section 2.6: Formatting dates ----
-# The correct date format for files in this instance is
-# dd/mm/yyyy
+# The correct date format for files in this instance is dd/mm/yyyy
 # Some files contain dates in the form dd-mm-yy
 # This function converts those dates into the correct format
 convert_dates <- function(df) {
@@ -141,5 +114,6 @@ convert_dates <- function(df) {
 }
 
 
+############################################################
 
 ### END OF SCRIPT ###
--- Data-Explorer/global.R
+++ Data-Explorer/global.R
@@ -1,26 +1,23 @@
-################################################################
-### Global
-### Data Explorer script 3 of 5
-###
-### Original Author: Jaime Villacampa
-### Original Date: October 2017
-###
-### This script includes all packages, datasets and other
-### values used by both the UI and server files
-
-
-
+############################################################
+## Code name - global.R
+## Data Release - Quarterly Data Explorer
+## Latest Update: Ruth Gordon, February 2022
+##
+## Written/run on - R Studio SERVER
+## R version - 3.6.1
+##
+## This script includes all packages, datasets and other
+## values used by both the UI and server files
+##
+############################################################
 ### Section 1: Housekeeping ----
-
 
 # 1.1 - Load libraries
 library(shiny)
 library(DT)
 library(googleVis)
 library(htmltools)
-#library(leaflet)
 library(plotly)
-# library(rgdal) (Only needed for mapping - not in use at present)
 library(dplyr)
 library(tidyr)
 library(readr)
@@ -29,7 +26,7 @@ library(lubridate)
 library(shinyWidgets)
 library(shinymanager)
 
-###Update each quarter
+## Update each quarter
 publication_date<-"30-november-2021"
 
 # 1.2 - Define filepath
@@ -39,7 +36,6 @@ publication_date<-"30-november-2021"
 rds_filepath <- ("./data/")
 
 # 1.3 - Credentials
-
 # Delete any existing credentials
 if(exists("credentials")) rm(credentials)
 
@@ -58,54 +54,39 @@ if (Sys.time() >= dmy_hm(paste(publication_date,"09:30"), tz=Sys.timezone())) {
   credentials <- list(authorisation = "Public")
 }
 
+
+############################################################
 ### Section 2: Loading Data ----
 
-
 # Beds data
-data_bed <- readRDS(paste0(
-  rds_filepath,
-  "beds.rds"))
+data_bed <- readRDS(paste0(rds_filepath, "beds.rds"))
 
 # Specialty data
-data_spec <- readRDS(paste0(
-  rds_filepath,
-  "spec.rds"))
+data_spec <- readRDS(paste0(rds_filepath, "spec.rds"))
 
 # SIMD data
-data_simd <- readRDS(paste0(
-  rds_filepath,
-  "simd.rds"))
+data_simd <- readRDS(paste0(rds_filepath, "simd.rds"))
 
 # Time Trend data
-data_trend <- readRDS(paste0(
-  rds_filepath,
-  "trend.rds")) %>%
+data_trend <- readRDS(paste0(rds_filepath, "trend.rds")) %>%
   mutate(quarter_middle = dmy(quarter_date) - ddays(45))
 
 # Population Pyramid data
-data_pyramid <- readRDS(paste0(
-  rds_filepath,
-  "pyramid.rds"))
+data_pyramid <- readRDS(paste0(rds_filepath, "pyramid.rds"))
 
 # Map data (outpatients)
 # Not in use at the moment
-# data_map_op <- readRDS(paste0(
-#   rds_filepath,
-#   "map_op.rds"))
+# data_map_op <- readRDS(paste0(rds_filepath, "map_op.rds"))
 
 # Cross-Boundary data
-data_cbf <- readRDS(paste0(
-  rds_filepath,
-  "cbf.rds"))
+data_cbf <- readRDS(paste0(rds_filepath, "cbf.rds"))
 
 
-
+############################################################
 ### Section 3: Other Values ----
-
 
 # All of these values are used for dropdown selections in different
 # parts of the app
-
 
 # 3.1 - Cross-Boundary diagram
 # Time trend data is used to get the two types of patients, but any
@@ -114,14 +95,12 @@ data_type <- data_trend %>%
   distinct(file) %>%
   pull(file)
 
-
 # 3.2 - Latest financial quarter
 # Again use time trend data
 latest_quarter <- data_trend %>%
   filter(quarter_date_last == max(quarter_date_last)) %>%
   distinct(quarter_date_last, quarter_name) %>%
   pull(quarter_name)
-
 
 # 3.3 - Dropdown choices in 'Table' tab
 file_types <-  c("Beds",
@@ -136,9 +115,7 @@ file_types <-  c("Beds",
                  "Outpatients - Specialty", 
                  "Outpatients - Deprivation (SIMD)")
 
-
 # 3.4 - Geography types
-
 
 # 3.4.1 - Excluding 'other' from SIMD tab
 # no longer want to present other in the SIMD tab 
@@ -148,7 +125,6 @@ geo_type <- data_trend %>%
   distinct(geo_type) %>%
   pull(geo_type)
 
-
 # 3.4.2 - Excluding 'Other'
 # Used in 'Time Trend' tab, as, at present, 'Other' is not
 # included as part of the time trend data
@@ -156,12 +132,10 @@ geo_type_trend <- data_trend %>%
   distinct(geo_type) %>%
   pull(geo_type)
 
-
 # 3.5 - Time Trend services
 trend_service <- data_trend %>%
   distinct(measure) %>%
   pull(measure)
-
 
 # 3.6 - Time Trend measures
 trend_measure <- c("Number of stays / appointments",
@@ -169,21 +143,18 @@ trend_measure <- c("Number of stays / appointments",
                    "Mean length of stay (days)",
                    "Did not attend rate (%)")
 
-
 # 3.7 - Pyramid services
 pyramid_service <- data_pyramid %>%
   distinct(measure) %>%
   pull(measure)
 
 
-
+############################################################
 ### Section 4: Plot Parameters ----
-
 
 # 4.1 - Time Trend colour palette
 trend_pal <- c("#004785", "#4c7ea9", "#99b5ce",
                "#00a2e5", "#4cbeed", "#99daf5")
-
 
 # 4.2 - Cross-Boundary colour palette for Sankey diagram
 # in the Cross-boundary tab
@@ -198,5 +169,6 @@ opts <- paste0("{
                }" )
 
 
+############################################################
 
 ### END OF SCRIPT ###
